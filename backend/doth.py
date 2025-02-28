03a11389f5dd38d4ba3e3de7768a9652c16450f3
Aswanth Vc <aswanth.abam@gmail.com>
2025-03-01 02:17:00 +0530

    fix(core): allow all cors

--- backend/doth.py
+++ backend/doth.py
@@ -15,10 +15,10 @@ application = FastAPI()
 
 application.include_router(router=api_router)
 
-
+print(settings.cors_origins)
 application.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=settings.cors_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
