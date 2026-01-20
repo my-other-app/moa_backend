6306696f52b89ae60660a4f286bab575ad99478e
Mohammed <muhammedr@Muhammeds-MacBook-Pro-3.local>
2026-01-20 21:20:32 +0530

    fix: Alembic migration down_revision to fix multiple heads
    - Set down_revision to 'a1b2c3d4e5f6' (latest migration)

--- backend/migrations/versions/add_event_id_notifications.py
+++ backend/migrations/versions/add_event_id_notifications.py
@@ -1,7 +1,7 @@
 """add event_id to notifications
 
 Revision ID: add_event_id_notifications
-Revises: 
+Revises: a1b2c3d4e5f6
 Create Date: 2026-01-20
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'add_event_id_notifications'
-down_revision = None
+down_revision = 'a1b2c3d4e5f6'
 branch_labels = None
 depends_on = None
 
